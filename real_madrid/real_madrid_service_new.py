0a608d5d3be774ad01daf0a4d308e9d8df16dd17
ataman <atamanenkoalex90@gmail.com>
2024-05-21 15:31:58 +0300

    upd real_madrid

--- real_madrid/real_madrid_service_new.py
+++ real_madrid/real_madrid_service_new.py
@@ -2,6 +2,7 @@ from DrissionPage import ChromiumPage, ChromiumOptions
 import time
 from variables import BASE_DIR
 from fake_useragent import UserAgent
+import random
 
 URL = "https://www.realmadrid.com/es-ES/entradas?filter-tickets=vip;general&filter-football=primer-equipo-masculino"
 
@@ -16,70 +17,77 @@ class RealMadridServiceNew:
         options = ChromiumOptions()
         options.headless(True)
         options.set_user_agent(user_agent=ua)
+        options.set_argument("--accept-lang=en-US")
 
         self.page = ChromiumPage(addr_or_opts=options)
+
+        self.queue_number = None
 
     def _get_url(self, url: str):
         print(f"Get url: {url}")
         print(f"User agent from page: {self.page.user_agent}")
         self.page.get(URL)
 
-    def _login(self):
+    def solve_capcha(self, tab):
+        iframe = tab.get_frame('@src^https://challenges.cloudflare.com/cdn-cgi')
+        i = 1
+        while iframe and i <= 3:
+            print("Find capcha iframe")
+            tab.get_screenshot(path=f"{BASE_DIR}/screenshots", name='real_madrid_capcha_tab.png',
+                                      full_page=True)
+            checkbox = iframe.ele('xpath:/html/body/div/div/div[1]/div/label/input')  # TODO: update be tag
+            checkbox.click()
+            print("Click to capcha checkbox")
+            time.sleep(i * random.uniform(20, 30))
+
+            iframe = tab.get_frame('@src^https://challenges.cloudflare.com/cdn-cgi')
+            i += 1
+            if not iframe:
+                break
+
+        return True
+
+    def go_to_tickets_page(self):
         print("Start login")
 
         self._get_url(url=self.login_url)
         time.sleep(10)
         print("Open init URL")
 
-        # TODO: https://github.com/g1879/DrissionPage/blob/master/docs_en/SessionPage/session_options.md
-        # cookie_bottom = WebDriverWait(self.driver, 10).until(
-        #     EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
-        # )
-        # self.driver.save_screenshot(f"{BASE_DIR}/screenshots/real_madrid_start_page.png")
-        # cookie_bottom.click()
-        # print("Click to cookie bottom")
-        #
-        # time.sleep(10)
-        # cookies = self.driver.get_cookies()
-        # if cookies:
-        #     for cookie in cookies:
-        #         self.driver.add_cookie(cookie)
-        # print("Set cookies")
+        cookie_bottom = self.page.ele('#onetrust-accept-btn-handler', timeout=5)
+        if cookie_bottom:
+            cookie_bottom.click()
+            print("Click to cookie bottom")
+            time.sleep(3)
 
         buy_ticket_button = self.page.ele(
             'xpath:/html/body/app-root/app-views/app-tickets/main/app-calendar-list/div/div/app-all-event-card[1]/article/main/footer/rm-button[2]/button')
         buy_ticket_button.click()
         print("Click to buy_ticket bottom")
 
-        capcha_tab = self.page.get_tab(0)
-
+        tickets_tab = self.page.get_tab(0)
         time.sleep(5)
-        print(f"Title new tab: {capcha_tab.title}")
 
-        iframe = capcha_tab.get_frame('@src^https://challenges.cloudflare.com/cdn-cgi')
-        if iframe:
-            print("Find capcha iframe")
-            capcha_tab.get_screenshot(path=f"{BASE_DIR}/screenshots", name='real_madrid_capcha_tab.png',
-                                      full_page=True)
-            checkbox = iframe.ele('xpath:/html/body/div/div/div[1]/div/label/input')  # TODO: update be tag
-            checkbox.click()
-            print("Click to capcha checkbox")
+        print("Checkout to new tab")
+        print(f"New tab title: {tickets_tab.title}")
 
-        time.sleep(30)
+        self.solve_capcha(tab=tickets_tab)
+
+        time.sleep(10)
         print("Open tickets tab")
-        print(f"Title new tab after capcha: {capcha_tab.title}")
-        capcha_tab.get_screenshot(path=f"{BASE_DIR}/screenshots", name='real_madrid_after_capcha.png', full_page=True)
-        print(f"Tab check url: {capcha_tab.url}")
-        print(f"Tab check html: {capcha_tab.html[:1000]}")
+        print(f"Title new tab after capcha: {tickets_tab.title}")
+        tickets_tab.get_screenshot(path=f"{BASE_DIR}/screenshots", name='real_madrid_after_capcha.png', full_page=True)
+        print(f"Tab check url: {tickets_tab.url}")
 
-        # Open a file in write mode
-        with open(f"{BASE_DIR}/screenshots/check_real_madrid.html", "w") as file:
-            # Write the HTML content to the file
-            file.write(capcha_tab.html)
 
+        # print(f"Tab check html: {tickets_tab.html[:1000]}")
+        # # Open a file in write mode
+        # with open(f"{BASE_DIR}/screenshots/check_real_madrid.html", "w") as file:
+        #     # Write the HTML content to the file
+        #     file.write(tickets_tab.html)
         # try:
-            # capcha_tab.wait.ele_displayed('.item-title', timeout=60)
-            # check_text = capcha_tab.ele('.item-title').text
+            # tickets_tab.wait.ele_displayed('.item-title', timeout=60)
+            # check_text = tickets_tab.ele('.item-title').text
         #     if check_text:
         #         print(f"Check text: {check_text}")
         #     else:
@@ -88,12 +96,62 @@ class RealMadridServiceNew:
         # except Exception as e:
         #     print("Element with class='item-title' didn't find")
 
+        return tickets_tab
+
+    def queue_wait(self, tab):
+        queue_info = tab.ele('.warning-box')
+        if queue_info:
+            queue_number = tab.ele('#MainPart_lbQueueNumber').text
+            self.queue_number = queue_number
+            print(f"You queue_number: {queue_number}")
+
+        while queue_info:
+            wait_minutes = tab.ele('#MainPart_lbWhichIsIn').text
+            online_users_count = tab.ele('#MainPart_lbUsersInLineAheadOfYou').text
+            print(f"Online users in front of you: {online_users_count}")
+            print(f"You will be logged in: '{wait_minutes}'")
+            print("Wait 60 seconds for next check...")
+            time.sleep(60)
+            queue_info = tab.ele('.warning-box')
+            if not queue_info:
+                break
+
+        self.solve_capcha(tab=tab)
+        return tab
+
+    def validate_member_number(self, tab):
+        # TODO: Debug
+        time.sleep(5)
+
+        cookie_buttons = tab.eles('.cookie-button')
+        if cookie_buttons:
+            cookie_buttons[1].click()
+            time.sleep(5)
+
+        reveal_modal_dialog = tab.ele('.reveal-modal-dialog')
+        if reveal_modal_dialog:
+            member_user = "test_user"
+            member_password = "test_password"
+
+            member_user_input = tab.ele('.memberUser')
+            member_password_input = tab.ele('.memberPwd')
+            validate_botton = tab.ele('.sale-member-submit')
+
+            member_user_input.input(member_user)
+            member_password_input.input(member_password)
+            validate_botton.click()
+
+            time.sleep(5)
+
+        return tab
+
     def run(self):
-        self._login()
+        tickets_tab = self.go_to_tickets_page()
+        tickets_tab = self.queue_wait(tab=tickets_tab)
+        tickets_tab = self.validate_member_number(tab=tickets_tab)
         time.sleep(10)
         self.page.close()
         self.page.quit()
-
 
 if __name__ == '__main__':
     try:
